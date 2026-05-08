4bdb6e476b81073fc04a0eea29b1886b68632a22
Mahir Isikli <isikli.mahir@gmail.com>
2026-05-08 16:29:24 +0200

    chore: migrate to earendil pi packages

--- extensions/index.ts
+++ extensions/index.ts
@@ -3,7 +3,7 @@ import { access, stat } from "node:fs/promises";
 import path from "node:path";
 import { promisify } from "node:util";
 
-import type { ExtensionAPI, ExtensionContext } from "@mariozechner/pi-coding-agent";
+import type { ExtensionAPI, ExtensionContext } from "@earendil-works/pi-coding-agent";
 import { Type } from "typebox";
 
 const execFileAsync = promisify(execFile);
